5a7b1569bb7182744de4363de1710bee829f431d
sorrowbeaver <jkj8790@gmail.com>
2018-10-14 23:32:36 +0900

    Solved until ten.

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle8.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle8.kt
@@ -1,6 +1,7 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
 
 object Riddle8 {
   /**
@@ -9,6 +10,8 @@ object Riddle8 {
    * Use case: Make an Observable "lazy" for some time. For instance, when wanting to postpone some UI action.
    */
   fun solve(source: Observable<Unit>): Observable<Unit> {
-    TODO()
+    // https://stackoverflow.com/questions/49476105/rxjava-what-is-the-difference-between-delay-and-delaysubscription?rq=1
+    // "This include subscribing" : The reason why use delaySubscription
+    return source.delaySubscription(200, TimeUnit.MILLISECONDS)
   }
 }
